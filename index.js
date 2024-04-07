be8f74f26b0275e59e712c1ccabe4c0745b1695f
Moncef Benboubakeur <42694745+moncefbenboubakeur@users.noreply.github.com>
2024-04-07 02:29:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -32,6 +32,21 @@ app.get("/create", (req, res) => {
   });
 });
 
+function printPosts() {
+  for (var i = 0; i < posts.length; i++) {
+    console.log(
+      "Posts array " +
+        [i] +
+        " " +
+        posts[i].postName +
+        " " +
+        posts[i].postDate +
+        " " +
+        posts[i].postContent
+    );
+  }
+}
+
 // This route handles the form submission.
 app.post("/create", (req, res) => {
   const { postName, postDate, postContent } = req.body;
@@ -39,6 +54,8 @@ app.post("/create", (req, res) => {
 
   if (postName) {
     console.log("Valid postName, redirecting...");
+    posts.push(req.body);
+    printPosts();
     res.redirect(`/success?postName=${encodeURIComponent(postName)}`);
   } else {
     console.log("Missing postName, rendering form again...");
@@ -49,6 +66,11 @@ app.post("/create", (req, res) => {
 app.get("/success", (req, res) => {
   const postName = req.query.postName; // This captures the 'postName' from the query string
   res.render("success", { postName: postName });
+});
+
+app.get("/view", (req, res) => {
+  console.log("Loaded posts:", posts);
+  res.render("view.ejs", { listPosts: posts });
 });
 
 app.listen(port, () => {
